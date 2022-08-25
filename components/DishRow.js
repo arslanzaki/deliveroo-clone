558e31780a8fa0db04faea95df3bf5034edd2680
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-25 11:59:13 +0500

    Add & Remove From Basket Added

--- components/DishRow.js
+++ components/DishRow.js
@@ -4,16 +4,25 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import { urlFor } from "../sanity";
 import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
 import { useDispatch, useSelector } from "react-redux";
-import { addToBasket } from "../features/basketSlice";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsById,
+} from "../features/basketSlice";
 
-const DishRow = ({ name, description, price, image }) => {
+const DishRow = ({ id, name, description, price, image }) => {
   const [isPressed, setIsPressed] = useState(false);
-  const basketItems = useSelector((state) => state.basket.items);
+  const basketItems = useSelector((state) => selectBasketItemsById(state, id));
   const dispatch = useDispatch();
 
   const addItemToBasket = () => {
-    dispatch(addToBasket({ name, description, price, image }));
+    dispatch(addToBasket({ id, name, description, price, image }));
   };
+  const removeItemFromBasket = () => {
+    if (!basketItems.length > 0) return;
+    dispatch(removeFromBasket({ id }));
+  };
+  console.log(basketItems);
   return (
     <>
       <TouchableOpacity
@@ -42,7 +51,7 @@ const DishRow = ({ name, description, price, image }) => {
       {isPressed ? (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity>
+            <TouchableOpacity onPress={removeItemFromBasket}>
               <MinusCircleIcon color="#00CCBB" size={40} />
             </TouchableOpacity>
             <Text>{basketItems.length}</Text>
--- features/basketSlice.js
+++ features/basketSlice.js
@@ -11,7 +11,18 @@ export const basketSlice = createSlice({
       state.items = [...state.items, action.payload];
     },
     removeFromBasket: (state, action) => {
-      state.value -= 1;
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      let newBasket = [...state.items];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't Remove Product (id: ${action.payload.id}) As Its Not In The Basket`
+        );
+      }
+      state.items = newBasket;
     },
   },
 });
@@ -20,5 +31,6 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // export const selectBasketItems = (state) => state.basket.items;
-
+export const selectBasketItemsById = (state, id) =>
+  state.basket.items.filter((item) => item.id === id);
 export default basketSlice.reducer;
