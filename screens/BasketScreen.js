9d24f0d738fd8ce38316e457e09ea89e5aaac0ad
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-30 12:38:00 +0500

    Basket Screen Half Done

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,19 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
 import { useMemo } from "react";
 import { selectRestaurant } from "../features/restaurantSlice";
+import { XCircleIcon } from "react-native-heroicons/solid";
+import { urlFor } from "../sanity";
+import { removeFromBasket } from "../features/basketSlice";
 
 const BasketScreen = () => {
   const navigation = useNavigation();
@@ -24,14 +34,47 @@ const BasketScreen = () => {
   //console.log(groupedItemsInBasket);
   console.log(restaurant.title);
   return (
-    <SafeAreaView>
-      <View>
-        <View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00BBCC] bg-white shadow-xs">
           <View>
             <Text className="text-center font-bold text-lg">Basket</Text>
             <Text className="text-center">{restaurant.title}</Text>
           </View>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
         </View>
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver In 30-45 Minutes</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00BBCC]">Change</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView className="divide-y divide-gray-200">
+          {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+            <View key={key} className="flex-row items-center space-x-3 bg-white py-2 px-5">
+              <Text className="text-[#00BBCC]">{items.length} x</Text>
+              <Image
+                source={{ uri: urlFor(items[0].image).url() }}
+                className="h-12 w-12 rounded-full"
+              />
+              <Text className="flex-1">{items[0].name}</Text>
+              <Text className="text-gray-600">Rs. {items[0].price}</Text>
+              <TouchableOpacity onPress={()=> dispatch(removeFromBasket({id: key}))}>
+                <Text className="text-[#00BBCC] text-xs">Remove</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
